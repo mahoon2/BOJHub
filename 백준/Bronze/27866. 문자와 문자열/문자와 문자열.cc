552a7b71102aa93c946bf6b48101b6a1c8c543e2
mahoon2 <mahoon268@gmail.com>
2023-09-14 16:30:18 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string inp;
+    int i;
+    cin >> inp;
+    cin >> i;
+    cout << inp[i-1] << '\n';
+    return 0;
+}
