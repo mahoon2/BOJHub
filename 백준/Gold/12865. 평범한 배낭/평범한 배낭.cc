c411592c556c9c76e02b9c24fa6c0dc4e7f711e0
mahoon2 <mahoon268@gmail.com>
2023-10-10 22:21:32 +0900

    [Gold V] Title: 평범한 배낭, Time: 8 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -15,7 +15,7 @@ using namespace std;
 
 int n, k;
 vector<pii> things;
-int dp[101][100001];
+int dp[2][100001];
 
 int main()
 {
@@ -26,30 +26,36 @@ int main()
 
     cin >> n >> k;
     int temp1, temp2;
-    for(int i=0; i<n; i++)
-	{
-		cin >> temp1 >> temp2;
-		things.push_back(make_pair(temp1, temp2));
-	}
-	
+    things = vector<pii>(n);
+    for(auto& p: things)
+    {
+        cin >> p.fi >> p.se;
+    }
+    int weight, cost;
+    cost = things[0].second;
+    
 	for(int i=0; i<things[0].first; i++)
 		dp[0][i] = 0;
 	for(int i=things[0].first; i<=k; i++)
-		dp[0][i] = things[0].second;
+		dp[0][i] = cost;
     
     for(int i=1; i<n; i++)
     {
-        for(int j=0; j<things[i].first; j++)
+        temp1 = i%2; temp2 = (i-1)%2;
+        weight = things[i].first;
+        cost = things[i].second;
+
+        for(int j=0; j<weight; j++)
         {
-            dp[i][j] = dp[i-1][j];
+            dp[temp1][j] = dp[temp2][j];
         }
-        for(int j=things[i].first; j<=k; j++)
+        for(int j=weight; j<=k; j++)
         {
-            dp[i][j] = max(dp[i-1][j], dp[i-1][j-things[i].first] + things[i].second);
+            dp[temp1][j] = max(dp[temp2][j], dp[temp2][j-weight] + cost);
         }
     }
 
-    cout << dp[n-1][k] << '\n';
+    cout << dp[(n-1)%2][k] << '\n';
     
     return 0;
 }
