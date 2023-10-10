7c1e28b982c057e3c2c84695f4a671fb9bc7676f
mahoon2 <mahoon268@gmail.com>
2023-10-10 22:17:21 +0900

    [Gold V] Title: 평범한 배낭, Time: 40 ms, Memory: 41476 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,32 +1,32 @@
-#include <iostream>
-#include <vector>
-#include <utility>
-#include <cstring>
+#include <bits/stdc++.h>
+
+#define INF 987654321
+#define fi first
+#define se second
+#define pii pair<int,int>
+
+#define FOR(i, n) for(int i=0; i<(n); i++)
+#define all(v) (v).begin(), (v).end()
+
+using ll = long long;
+using ld = long double;
 
 using namespace std;
 
 int n, k;
-vector<pair<int, int>> things;
-bool ispicked[100] = {false};
-int dp[100][100001];
-
-int solve(int pick, int weight)
-{
-	if(pick < 0) return 0;
-	int& ret = dp[pick][weight];
-	if(ret != -1) return ret;
-	
-	if(things[pick].first > weight)
-		return ret = solve(pick-1, weight);
-	return ret = max(solve(pick-1, weight-things[pick].first) + things[pick].second, solve(pick-1, weight));
-}
+vector<pii> things;
+int dp[101][100001];
 
 int main()
 {
-	memset(dp, -1, sizeof(dp));
-	cin >> n >> k;
-	int temp1, temp2;
-	for(int i=0; i<n; i++)
+    cin.tie(NULL); cout.tie(NULL); ios_base::sync_with_stdio(false);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+    #endif
+
+    cin >> n >> k;
+    int temp1, temp2;
+    for(int i=0; i<n; i++)
 	{
 		cin >> temp1 >> temp2;
 		things.push_back(make_pair(temp1, temp2));
@@ -36,7 +36,20 @@ int main()
 		dp[0][i] = 0;
 	for(int i=things[0].first; i<=k; i++)
 		dp[0][i] = things[0].second;
-	
-	cout << solve(n-1, k) << '\n';
-	return 0;
+    
+    for(int i=1; i<n; i++)
+    {
+        for(int j=0; j<things[i].first; j++)
+        {
+            dp[i][j] = dp[i-1][j];
+        }
+        for(int j=things[i].first; j<=k; j++)
+        {
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-things[i].first] + things[i].second);
+        }
+    }
+
+    cout << dp[n-1][k] << '\n';
+    
+    return 0;
 }
